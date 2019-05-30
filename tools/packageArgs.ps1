ac5b2d5f859090ff8f8756bf6411cc2cda79a48b
RB <RedBaron2@users.noreply.github.com>
2019-05-30 15:57:21 -0700

    Update packageArgs.ps1
    error due to ()

--- tools/packageArgs.ps1
+++ tools/packageArgs.ps1
@@ -1,6 +1,6 @@
 ﻿# Due to a bug in AU. This package requires that all package Arguments be in this hashtable
 
-$ProgramFiles = @{$true="%PROGRAMFILES%";$false="%PROGRAMFILES(X86)%"}[ ( Get-ProcessorBits -eq 64 ) ]
+$ProgramFiles = @{$true="%PROGRAMFILES%";$false="%PROGRAMFILES(X86)%"}[ ((Get-ProcessorBits) -eq '64') ]
 $Dir = @{$true="${PROGRAMFILES}\AdoptOpenJDK";$false="%PROGRAMFILES%\AdoptOpenJDK"}[ ( Test-Path "$ProgramFiles" ) ]
 
 $packageArgs = @{
