b113c76f6857ba16e800cc5adafe3af46494b36e
RB <RedBaron2@users.noreply.github.com>
2019-06-04 14:11:48 -0700

    Update chocolateyinstall.ps1
    update code

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,11 +1,17 @@
-﻿
+
   $scriptDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-  # Import function to test if JRE in the same version is already installed
+  # Import function to test if JRE is the same version as already installed
   . (Join-Path $scriptDir 'packageArgs.ps1')
 
 Install-ChocolateyZipPackage @packageArgs
 
+$installed = ( Get-InstalledArgs )
+
+# JAVA_HOME enviromental should only be set for JDK installs per https://docs.oracle.com/javase/8/docs/technotes/guides/troubleshoot/envvars001.html#CIHEEHEI
+if (($installed -notmatch 'jre')){
+# return from function as hash then assign variables
 Install-ChocolateyEnvironmentVariable "JAVA_HOME" "${targetDir}\${installed}" "Machine"
 
 # The full path instead of the %JAVA_HOME% is needed so it can be removed with the Chocolatey Uninstall
 Install-ChocolateyPath "${targetDir}\${installed}\bin" -PathType "Machine"
+}
