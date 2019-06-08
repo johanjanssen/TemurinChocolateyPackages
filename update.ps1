161f0c32422891f2f53eeab6e54bf2cb0ebbff40
RB <RedBaron2@users.noreply.github.com>
2019-06-07 21:18:45 -0700

    Update update.ps1
    Clean up of code and updated for nightly builds

--- update.ps1
+++ update.ps1
@@ -34,29 +34,55 @@ param (
     [string]$jvm = 'hotspot'
 )
 
+    $regex_1 = "(\d{4}\-\d{2}\-\d{2}\-\d{2}\-\d{2})"
+    $regex_2 = "(OpenJDK(\d{1,2}U|\d{1,2}\.\d\.)\-(jdk|jre)_x(64|86\-32)_([wndois]+)_([htosp]+|[openj9]+))_|(_[openj9]+\-.*)|(\.zip)"
     $releases = "https://api.adoptopenjdk.net/v2/info/${build}/openjdk${number}?openjdk_impl=${jvm}&os=windows&arch=x32&arch=x64&release=latest&type=${type}"
+    $t =  try { 
+    (Invoke-WebRequest -Uri $releases -ErrorAction Stop -UseBasicParsing).BaseResponse
+    } catch [System.Net.WebException] { Write-Verbose "An exception was caught: $($_.Exception.Message)"; $_.Exception.Response }
+    if ( $t.StatusCode -eq "OK" ) {    
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing | ConvertFrom-Json
     $urls = $download_page.binaries.binary_link | where { $_ -match "x64|x86"} | select -Last 6
 
     $url32 = $urls | where { $_ -match "x86"} | select -Last 1
 
     $url64 =  $urls | where { $_ -match "x64"} | select -Last 1
+    
+    } else { Write-Verbose "this is a bad request"; }
 
-		if ($url32 -match '(\du)(\d+){3}(b)(\d+){2,3}') {
-		$version = ( $Matches[0] ) -replace('[u]','.0.') -replace('(b)','.')
-		}
-		if (( $number -eq 9 ) -or ( $number -eq 10 ) -or ( $number -eq 11 )-or ( $number -eq 12 )) {
-		$version = ( Get-Version (($url64) -replace('%2B','.')) )
-		}
+        if ($build -eq "nightly") {
+        $fN = ($download_page.binaries.binary_name | Select -First 1 )
+        $version = ( $fN -split "$regex_1" | select -Last 2 | Select -First 1 )
+        } else {
+        if ($number -eq 8) {
+        $name = ( $download_page.binaries.binary_name ) | Select -First 1
+        $name = ( $name ) -replace(".zip",'')
+        $fN = ( $name )
+            if ( $jvm -eq 'openj9' ) {
+              $version = (( $fN -split "$regex_2" ) )
+              $version = ( $version | Select -Last 3 )
+              $version = $version -replace("(_[openj9]+(_|\-.*))",'')
+              $version = ( $version ) -replace("(`r`n\s)",'G') | Select -First 1
+            } else {
+              $version = (( $fN -split "$regex_2" ) | Select -Last 1 )
+            }
+        $version = $version -replace('[u]','.0.') -replace('(b)','.')
+        }
+		    if (( $number -eq 9 ) -or ( $number -eq 10 ) -or ( $number -eq 11 )-or ( $number -eq 12 )) {
+		    $version = if ($url64 -ne $null) { ( Get-Version (($url64) -replace('%2B','.')) ) }
+		    }
+        }
+
 		$JavaVM = @{$true="${type}${number}";$false="${type}${number}-${jvm}"}[ ( $jvm -match "hotspot" ) ]
+        $beta = @{$true="${version}";$false="${version}-${build}"}[ ($build -eq "releases") ]
 
     #build stream hashtable return
     $hotspot = @{}
         if ($url32 -ne $null) { $hotspot.Add( 'URL32', $url32 ) }
         if ($url64 -ne $null) { $hotspot.Add( 'URL64', $url64 ) }
-        $hotspot.Add( 'Version', $version )
+        if ($version -ne $null) { $hotspot.Add( 'Version', "$beta" )
         $hotspot.Add( 'Title', "AdoptOpenJDK ${type}${number} ${jvm} ${version}" )
-        $hotspot.Add( 'PackageName', "AdoptOpenJDK-${JavaVM}" )
+        $hotspot.Add( 'PackageName', "AdoptOpenJDK-${JavaVM}" ) }
 
     return ( $hotspot )
 }
