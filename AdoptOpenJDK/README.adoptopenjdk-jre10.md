e67b249304156979813d0cd894f173c0e3eda718
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-08-02 13:15:15 +0200

    Moved files to separate folder for au

---
+++ AdoptOpenJDK/README.adoptopenjdk-jre10.md
@@ -0,0 +1,5 @@
+# [<img src="AdoptOpenJDK.png" width="48" height="48"/> AdoptOpenJDK_JRE10](https://chocolatey.org/packages/kfa)
+
+Prebuilt OpenJDK Binaries for Free!
+Java™ is the world's leading programming language and platform. AdoptOpenJDK uses infrastructure, build and test scripts to produce prebuilt binaries from OpenJDK™ class libraries and a choice of either the OpenJDK HotSpot or Eclipse OpenJ9 VM.
+All AdoptOpenJDK binaries and scripts are open source licensed and available for free. This package uses OpenJDK HotSpot for JRE10
---
+++ AdoptOpenJDK/tools/packageArgs.ps1
@@ -0,0 +1,26 @@
+﻿# Due to a bug in AU. This package requires that all package Arguments be in this hashtable
+
+$Dir = @{$true="${env:PROGRAMFILES}";$false="${env:PROGRAMFILES(X86)}"}[ ((Get-OSArchitectureWidth 64) -and $env:chocolateyForceX86 -ne $true)]
+# $Dir = @{$true="C:\Program Files";$false="${env:PROGRAMFILES}"}[ ( Test-Path "$ProgramFiles" ) ]
+
+$packageArgs = @{
+  PackageName = ''
+  Url = ''
+  UnzipLocation = "$Dir\AdoptOpenJDK"
+  Url64bit = ''
+  Checksum = ''
+  ChecksumType = ''
+  Checksum64 = ''
+  ChecksumType64 = ''
+}
+
+$targetDir = ( $packageArgs['UnzipLocation'] )
+
+function Get-InstalledArgs {
+$regex_hotspot = "(?:[jdkre]+)\-\d+\.\d\.\d+(.)\d+\-(?:[jdkrehotp]+)"
+$regex = @{$true="(?:[jdkre]+)\d[u]\d+\-[b]\d+\-(?:[jdkre]+)";$false="(?:[jdkre]+)\-\d+\.\d\.\d+(.)\d+\-(?:[jdkre]+)"}[ (($packageArgs['PackageName'] -replace('[a-zA-Z]+\-[a-zA-Z]+','')) -eq "8")]
+$build = @{$true="$regex_hotspot";$false="$regex"}[ ($packageArgs['PackageName'] -match "hotspot")]
+$arr = Get-ChildItem $targetDir | Where-Object {$_.PSIsContainer} | Foreach-Object {$_.Name -match $build}
+$installed = @{$true=$Matches[0];$false="WeAreBad"}[ ($Matches[0] -ne "") ]
+return ( $installed )
+}
