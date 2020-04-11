2bbd5375d0946c0a9c5c1e00e2dcee49bac37c46
RB <RedBaron2@users.noreply.github.com>
2020-04-10 18:23:07 -0700

    Update install32.ps1
    blanking the vars

--- AdoptOpenJDK/install32.ps1
+++ AdoptOpenJDK/install32.ps1
@@ -7,14 +7,14 @@ $toolsDir = @{$true="${env:ProgramFiles}\AdoptOpenJDK";$false="${env:programfile
 $parameters = (Get-PackageParameters); $pp = ( Test-PackageParamaters $parameters ).ToString() -replace('\=""\;','')
 
 $packageArgs = @{
-  PackageName = 'AdoptOpenJDK-jre8'
-  fileType = 'msi'
-  Url = 'https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u-2020-04-02-00-08/OpenJDK8U-jre_x86-32_windows_hotspot_2020-04-02-00-08.zip'
-  Url64bit = 'https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u-2020-04-02-00-08/OpenJDK8U-jre_x64_windows_hotspot_2020-04-02-00-08.zip'
-  Checksum = 'B276E0CDF8FF3003D8E94F6C65BA811F7FA84C5238FD2E6099360E439448F757'
-  ChecksumType = 'sha256'
-  Checksum64 = '8E1494FDC4CBD359231FA92BC4C6C71DACBCA4C2228279DF18AC4422DFD385AB'
-  ChecksumType64 = 'sha256'
+  PackageName = ''
+  fileType = ''
+  Url = ''
+  Url64bit = ''
+  Checksum = ''
+  ChecksumType = ''
+  Checksum64 = ''
+  ChecksumType64 = ''
   SilentArgs = "$pp"
 }
 
