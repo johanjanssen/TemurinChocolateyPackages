1a3bcf2d7ec6154e1222e6fa04e77d3ed2df377c
RB <RedBaron2@users.noreply.github.com>
2020-06-04 15:28:18 -0700

    Update install64.ps1
    fix for params by regex changes

--- AdoptOpenJDK/install64.ps1
+++ AdoptOpenJDK/install64.ps1
@@ -1,10 +1,10 @@
-﻿$ErrorActionPreference  = 'Stop'
+$ErrorActionPreference  = 'Stop'
  if(!$PSScriptRoot){ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
 . "$PSScriptRoot\helper.ps1"
 
 # Get Package Parameters
 $toolsDir = @{$true="${env:ProgramFiles}\AdoptOpenJDK";$false="${env:programfiles(x86)}\AdoptOpenJDK"}[ ((Get-OSArchitectureWidth 64) -or ($env:chocolateyForceX86 -eq $true)) ]
-$pp = ( Test-PackageParamaters (Get-PackageParameters) ).ToString() -replace('\=""\;','')
+$parameters = (Get-PackageParameters); $pp = ( Test-PackageParamaters $parameters ).ToString() -replace('"|="True"','') -replace(";", ' ') -replace("==", '=')
 
 $packageArgs = @{
   PackageName = ''
