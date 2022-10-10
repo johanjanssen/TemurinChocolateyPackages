98e635b3bec6e39cb81f6b79af8b683396fbe001
Johan Janssen <johanjanssen2001@gmail.com>
2022-10-10 07:40:58 +0200

    Update update.ps1

--- Temurin/update.ps1
+++ Temurin/update.ps1
@@ -145,7 +145,7 @@ if ($fixedVersion) {
 
 function global:au_GetLatest {
 # Skip 9 and 10 as they don't have MSI's
-$numbers = @("8", "11", "17","18"); $types = @("jdk","jre")
+$numbers = @("8", "11", "17","18","19"); $types = @("jdk","jre")
 # Optionally add "nightly" to $builds
 $jvms = @("hotspot"); $builds = @("ga"); $os = "windows"
 
